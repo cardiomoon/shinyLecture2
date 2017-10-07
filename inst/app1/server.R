3c576b2c64ebf3cc40a57a93de1cd159f5c5cb55
cardiomoon <cardiomoon@gmail.com>
2017-10-07 23:52:59 +0900

    2017-10-5
    2017-10-5

---
+++ inst/app1/server.R
@@ -0,0 +1,7 @@
+shinyServer(function(input, output) {
+  output$distPlot <- renderPlot({
+   
+    dist <- rnorm(input$obs)
+    hist(dist)
+  })
+}) 
---
+++ inst/app1/ui.R
@@ -0,0 +1,10 @@
+shinyUI(pageWithSidebar(
+  headerPanel("Click the button"),
+  sidebarPanel(
+    sliderInput("obs", "Number of observations:",
+                min = 0, max = 1000, value = 500)
+  ),
+  mainPanel(
+    plotOutput("distPlot")
+  )
+))
