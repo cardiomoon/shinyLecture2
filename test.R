c371c55bb784e08aa242e76d133aea962702464a
Keon-Woong Moon <cardiomoon@gmail.com>
2018-03-21 16:51:41 +0900

    2018-3-21
    2018-3-21

--- test.R
+++ test.R
@@ -1,15 +1,41 @@
 library(shiny)
-ui <- fluidPage(
-     sliderInput("obs","Select number",min=0,max=1000,value=500),
-     plotOutput("distPlot")
-)
-server <- function(input,output){
+
+testInput=function(id){
+      ns<-NS(id)
+      
+      uiOutput(ns("test"))
+}
+
+test=function(input,output,session){
+     
+     ns<-session$ns
+     output$test=renderUI({
+          tagList(
+               sliderInput(ns("obs"),"Select number",min=0,max=1000,value=500),
+               plotOutput(ns("distPlot")),
+               verbatimTextOutput(ns("text"))
+          )
+     })
      output$distPlot=renderPlot({
           hist(rnorm(input$obs))
      })
+     
+     output$text=renderPrint({
+          ns("text")
+     })
+}
+
+ui <- fluidPage(
+     
+     testInput("test1")
+     
+)
+server <- function(input,output){
+     
+     callModule(test,"test1")
 }
 shinyApp(ui=ui,server=server)
 
 
 
-sliderInput("obs","Select number",min=0,max=1000,value=500)
+
